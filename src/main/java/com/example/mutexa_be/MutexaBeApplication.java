98b041448662a679638386fef594a384a3617498
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2026-03-13 13:14:12 +0700

    first commit

---
+++ src/main/java/com/example/mutexa_be/MutexaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mutexa_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutexaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutexaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mutexa_be/MutexaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mutexa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutexaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
