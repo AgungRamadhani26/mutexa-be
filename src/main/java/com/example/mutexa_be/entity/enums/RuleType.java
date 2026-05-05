88321c5603f69af8c2a1fbf0c3ef181a67ca1495
AgungRamadhani26 <agungramadhani@students.undip.ac.id>
2026-05-05 10:21:36 +0700

    refactor: remove deprecated legacy configuration utility module

--- src/main/java/com/example/mutexa_be/entity/enums/RuleType.java
+++
@@ -1,7 +0,0 @@
-package com.example.mutexa_be.entity.enums;
-
-public enum RuleType {
-   NAME,
-   ACCOUNT_NUMBER,
-   KEYWORD
-}
--- src/main/java/com/example/mutexa_be/repository/ExclusionRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mutexa_be.repository;
-
-import com.example.mutexa_be.entity.ExclusionRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExclusionRuleRepository extends JpaRepository<ExclusionRule, Long> {
-}
